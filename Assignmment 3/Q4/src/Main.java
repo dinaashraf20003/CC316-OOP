88cb308312dd5f275894676b2b0fa8482eebc3b6
dinaashraf20003 <73821958+dinaashraf20003@users.noreply.github.com>
2023-03-23 19:53:09 +0200

    Delete Assignmment 3 directory

--- Assignmment 3/Q4/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Triangle t =new Triangle("Blue", true, 1 ,2 ,3);
-        System.out.println(t.getPerimeter());
-        System.out.println(t.getArea(2,3));
-        System.out.println(t.getColor());
-        System.out.println(t.isFilled());
-    }
-}
