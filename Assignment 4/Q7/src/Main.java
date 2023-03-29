53ee14f849322d4ff22b4f60dd4ca20387eafb90
dinaashraf20003 <73821958+dinaashraf20003@users.noreply.github.com>
2023-03-29 22:54:12 +0200

    Add files via upload

---
+++ Assignment 4/Q7/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("Heba.txt");
+        if (file.exists()) {
+            System.out.println("File already exists");
+            System.exit(0);
+        }
+
+        try (
+                PrintWriter output = new PrintWriter(file);
+        ) {
+            for (int i = 0; i < 100; i++) {
+                output.print(((int)(Math.random() * 500) + 1));
+                output.print(" ");
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        try(
+
+                Scanner input = new Scanner(file);
+        ) {
+            while (input.hasNext()) {
+                list.add(input.nextInt());
+            }
+        }
+
+        Collections.sort(list);
+        System.out.print(list.toString());
+        System.out.println();
+    }
+}
